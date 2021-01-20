e4337af0ac443fa12a2f290a630ad486482e9515
AlirezaRezaei1990 <a.r.rezaei@aut.ac.ir>
2021-01-20 14:00:07 +0330

    first change

---
+++ constant.py
@@ -0,0 +1,10 @@
+#Constants Variables
+#@
+gc = -10.0e-8        #A value for variables to be calculated
+gcompany = '"OTCE"'  #The name of design company
+gdesigner = '"A&N"'  #The name of desigener(s)
+gMaxGen = 10         #ga generation size
+gPopulSize = 49      #ga population size
+gSUBPoSize = 7       #ga subpopulation size
+gUseVect = True      #ga vectorized option <false> or <true>
+gCFXsolmth = 'Nini'  #simulation initialization method 'Nini' or 'Wini'
